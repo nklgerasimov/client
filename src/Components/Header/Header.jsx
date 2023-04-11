4b456f95f5929a0c5df59a0989435ef38006c7f1
nklgerasimov <gniko@yandex.ru>
2023-04-11 17:05:30 +0300

    init

---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import HeaderTop from './headerTop/HeaderTop'
+import HeaderMenu from './headerMenu/HeaderMenu'
+
+const Header = () => {
+  return (
+    <div>
+        <HeaderTop/>
+        <HeaderMenu/>
+    </div>
+  )
+}
+
+export default Header
---
+++ src/Components/Header/headerTop/HeaderTop.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './headerTop.css'
+
+const HeaderTop = () => {
+  return (
+    <div className="container">
+        <div className='header-top'>
+            <div className="phone">(786 519-4329)</div>
+            <div className="daySale">Labor day sale 30% <span>off+up to $239 in free gifts. Free delivery & returns! <span className='tt-none'>Promo:</span></span> As30</div>
+            <div className="faqs">FAQS</div>
+        </div>
+    </div>
+  )
+}
+
+export default HeaderTop
